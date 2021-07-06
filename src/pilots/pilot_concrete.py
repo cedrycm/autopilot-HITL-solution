352d4cc84192e6eeb6f2ee4665da08d2cf17554d
Cedryc Midy <cedmidy78@gmail.com>
2021-07-06 13:22:41 -0700

    "MANUAL" pilot implemented

--- src/pilots/pilot_concrete.py
+++ src/pilots/pilot_concrete.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 import struct
 import sys
-import time
+import keyboard
 
 from zip_sim import TELEMETRY_STRUCT, COMMAND_STRUCT
 
@@ -41,7 +41,7 @@ class ManualPilot(Pilot):
         pass
 
     @abstractmethod
-    def prepare_command(self):
+    def send_command(self):
         # abstract method for pilot to prepare command
         pass
 
@@ -81,11 +81,50 @@ class Autopilot1(AutoPilot):
 
 
 class ManualPilot1(ManualPilot):
+    _padding = "zip"
+
+    def __init__(self):
+        self._lateral_airspeed = 0
+        self._drop_package_commanded = 0
+        self._drop_timestamp = 0 
+        self._timestamp = 0
+
     # skeleton class for manualpilot implementation
-    def interpret_telemetry(self):
-        # skeleton method for pilot to interpret telemetry received
+    def interpret_telemetry(self, telemetry_buffer):
+        #save instance of timestamp
+        telemetry = TELEMETRY_STRUCT.unpack(telemetry_buffer)
+        self._timestamp = telemetry[0]
         return None
 
-    def prepare_command(self):
-        # abskeletonstract method for pilot to prepare command
+    def send_command(self):
+        self._lateral_airspeed -= self._lateral_airspeed / 0.5 * (1 / 60.0)
+        if keyboard.is_pressed("a"):
+            self._lateral_airspeed = min(
+                30.0, self._lateral_airspeed + (1 / 60.0) * 200.0
+            )
+        if keyboard.is_pressed("d"):
+            self._lateral_airspeed = max(
+                -30.0, self._lateral_airspeed - (1 / 60.0) * 200.0
+            )
+        if keyboard.is_pressed("space"):
+            time_elapsed = self._timestamp - self._drop_timestamp
+            if time_elapsed > 500:
+                self._drop_package_commanded = 1
+                self._drop_timestamp = self._timestamp
+
+        cmd = COMMAND_STRUCT.pack(
+            self._lateral_airspeed,
+            self._drop_package_commanded,
+            self._padding.encode(),
+        )
+
+        # send command back to parent process
+        try:
+            sys.stdout.buffer.write(cmd)
+            sys.stdout.flush()
+        except struct.error as e:
+            raise e
+
+        self._drop_package_commanded = 0
+
         return None
